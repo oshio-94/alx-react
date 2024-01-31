29ef810895b7d93f651bb8ba5c8213528e21aa3d
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-31 14:24:50 +0100

    new task

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
+import HolbertonLogo from './Holberton-logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={HolbertonLogo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
       </header>
       <body className="App-body">
         <p>Login to access the full dashboard</p>
