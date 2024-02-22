f154024acfbdcf358fb6f9bb9487f927f3bcfae9
John Ikhanoba <oshioi@yahoo.co.uk>
2024-02-22 09:26:00 +0100

    task 5

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,10 +1,10 @@
 import { List, Map } from 'immutable';
 
-function concatElements (page1, page2) {
+function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
-function mergeElements (page1, page2) {
+function mergeElements(page1, page2) {
   return Map(page1).merge(page2);
 }
 
