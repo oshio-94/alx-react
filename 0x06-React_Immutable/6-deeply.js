5b6c451f6cdf2a3f6a2d1ced222d8e0e5ef601c5
John Ikhanoba <oshioi@yahoo.co.uk>
2024-02-22 09:35:14 +0100

    task 6

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,5 @@
 import { Map } from 'immutable';
 
-export default function mergeDeeplyElements (page1, page2) {
-  const mapOne = new Map(page1);
-  const mapTwo = new Map(page2);
-
-  return mapOne.mergeDeep(mapTwo);
+export default function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
 }
